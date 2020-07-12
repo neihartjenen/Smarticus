fb8fc2da8922667f849b3b67c05cabae3dcb8f96
jenenneihart <neihart.jenen@gmail.com>
2020-07-12 14:12:51 -0700

    getting everything onto my login branch

---
+++ reactpractice/src/pages/login.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Login extends Component{
+    state={
+        username: "",
+        password: "",
+    };
+
+    render() {
+        return(
+            <div>
+                Hello
+            </div>
+        )
+      }
+      
+      }
+      
+      export default Login;
