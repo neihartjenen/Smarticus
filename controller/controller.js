ec358bc0d60c4e70dad68ca9b135b41c66bddfc1
Cameronmbell99 <cameronbellbchs@gmail.com>
2020-07-13 18:44:35 -0500

    brought back the code for the controller

---
+++ controller/controller.js
@@ -0,0 +1,13 @@
+const db = require("../models");	
+const { model } = require("../models/User");	
+
+model.exports = {	
+    findAll: function(req, res){	
+        db.Question.find(req.query).sort({_id: -1}).then(dbModel => res.json(dbModel).catch(err => res.status(422).json(err)));	
+
+    },	
+
+    create: function(req, res ){	
+        db.User.create(req.body).then(dbModel => res.json(dbModel)).catch(err => res.status(422).json(err));	
+    }	
+}
